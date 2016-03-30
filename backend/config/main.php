66688e298b7ad08010a14a2dc246dddab0a99d5a
wuzhc <wzc1716220125@163.com>
2016-03-30 22:25:48 +0800

    后台配置文件

---
+++ backend/config/main.php
@@ -0,0 +1,40 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'defaultRoute' => 'admin/index', //默认控制器
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+    'components' => [
+        'user' => [
+            'identityClass' => 'backend\models\Admin',
+            'enableAutoLogin' => true,
+            'loginUrl' => ['admin/login'], //设置未登录是的跳转地址
+        ],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],
+        'assetManager' => [
+            'basePath' => '@webroot/backend/web/assets',
+            'baseUrl' => '@web/backend/web/assets'
+        ],
+    ],
+    'params' => $params,
+];
