520ada4d84e5b82e53f1057aaa31190215e085ad
wuzhc <wzc1716220125@163.com>
2016-10-09 20:28:05 +0800

    test coreseek

--- test.php
+++ test.php
@@ -4,11 +4,12 @@ require("sphinxapi.php");
 
 $cl = new SphinxClient ();
 
-$cl->SetServer ( 'localhost', 9312);
+$s_key = $_GET['keyword'] ?: 'two';
+$cl->SetServer( 'localhost', 9312);
 $cl->SetConnectTimeout ( 3 );
 $cl->SetArrayResult ( true );
 $cl->SetMatchMode ( SPH_MATCH_ANY);
-$res = $cl->Query ( $s_key, "two" );
+$res = $cl->Query ( $s_key, "*" );
 
 echo '<pre>';
 print_r($res);
