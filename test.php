032d4ab8dc1cda8f5463d87e969006298ae6b105
wuzhc <1716220125@qq.com>
2016-10-10 08:56:04 +0800

    test sphinx

--- test.php
+++ test.php
@@ -11,10 +11,22 @@ $cl->SetArrayResult ( true );
 $cl->SetMatchMode ( SPH_MATCH_ANY);
 $res = $cl->Query ( $s_key, "*" );
 
+$data = [];
+$data['total'] = $res['total_found'];
+$data['time'] = $res['time'];
+$data['keyword'] = $res['words'];
+
+$result = $res['matches'];
+foreach ($result as $r) {
+    $temp = [];
+    $temp['id'] = $r['id'];
+    foreach ($r['attrs'] as $key => $attr) {
+        $temp[$key] = $attr;
+    }
+    $data['data'][] = $temp;
+}
+
 echo '<pre>';
-print_r($res);
+print_r($data);
 echo '</pre>';
-echo '<hr>';
-echo '<pre>';
-print_r($cl);
-echo '</pre>';
+
