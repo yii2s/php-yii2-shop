679d5d87c35c82ff5d1c63fab69673867f4e1b69
wuzhc <wzc1716220125@163.com>
2016-10-30 21:16:40 +0800

    change document mode

--- frontend/views/layouts/main_login.php
+++ frontend/views/layouts/main_login.php
@@ -12,7 +12,8 @@ common\assets\AdminLteAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1">-->
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
